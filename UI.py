734f957aedb6b532f0902c8df6edb93019b7a7d4
Michel Desjardins <nitrome264@gmail.com>
2022-01-19 12:24:37 -0500

    leger resize

--- UI.py
+++ UI.py
@@ -6,7 +6,7 @@ layout = [
     [sg.Text('File 2'), sg.InputText(), sg.FileBrowse(),
      sg.Checkbox('SHA256')
      ],
-    [sg.Output(size=(88, 20))],
+    [sg.Output(size=(90, 20))],
     [sg.Submit(), sg.Cancel()]
 ]
 window = sg.Window('File Compare', layout)
