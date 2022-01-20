eefb3d065430a977f1e2b9eca7f29e24ade5e5f2
Michel Desjardins <nitrome264@gmail.com>
2022-01-20 14:33:08 -0500

    plus adapté

--- UI.py
+++ UI.py
@@ -1,29 +1,64 @@
 import PySimpleGUI as sg
+import control
 
 layout = [
     [
-        sg.Text("Température"),
-        sg.InputText(),
+        sg.Text("Température (°C)"),
+        sg.Slider(
+            range=(0, 35),
+            default_value=20,
+            size=(20, 15),
+            orientation="horizontal",
+            font=("Helvetica", 12),
+        ),
+        sg.Button("Modifier la température"),
     ],
-    [sg.Text("CO2"), sg.InputText()],
-    [sg.Text("Humidité"), sg.InputText()],
-    [sg.Output(size=(90, 20))],
+    [
+        sg.Text("CO2 (%)"),
+        sg.Slider(
+            range=(0, 35),
+            default_value=20,
+            size=(20, 15),
+            orientation="horizontal",
+            font=("Helvetica", 12),
+        ),
+        sg.Button("Modifier le CO2"),
+    ],
+    [
+        sg.Text("Humidité (%)"),
+        sg.Slider(
+            range=(0, 35),
+            default_value=20,
+            size=(20, 15),
+            orientation="horizontal",
+            font=("Helvetica", 12),
+        ),
+        sg.Button("Modifier l'humidité"),
+    ],
+    [sg.Output(size=(80, 20))],
     [sg.Button("Soumettre"), sg.Cancel()],
 ]
 
 
-def isNumber(caracter):
-    try:
-        n = int(caracter)
-        return True
-    except ValueError:
-        return False
+def allNumValues(caracters):
+    for i in range(len(caracters)):
+        try:
+            n = int(caracters[i])
+        except ValueError:
+            return False
+    return True
 
 
-window = sg.Window("Contrôle de la serre", layout)
+window = sg.Window("Contrôle de la serre", layout, element_justification="c")
 while True:  # The Event Loop
     event, values = window.read()
     if event in (None, "Exit", "Cancel"):
         break
-    elif event == "Soumettre" and isNumber(values[0]) == True:
+    elif event == "Modifier la température":
+        control.updateTemp(values[0])
+    elif event == "Modifier le CO2":
+        control.updateCO2(values[1])
+    elif event == "Modifier l'humidité":
+        control.updateHumidity(values[2])
+    elif event == "Soumettre" and allNumValues(values) == True:
         print(values)
---
+++ control.py
@@ -0,0 +1,10 @@
+def updateTemp(newTamp):
+    print(f"Nouvelle tempéreture : {newTamp}")
+
+
+def updateCO2(newCO2):
+    print(f"Nouveau taux de CO2 : {newCO2}")
+
+
+def updateHumidity(newHumid):
+    print(f"Nouveau taux d'humidité : {newHumid}")
