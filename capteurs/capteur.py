59ddbaa9a29048d3d02a86430fd05e9196f8afea
labonnesauce <oli.lajoie@gmail.com>
2022-02-24 10:04:45 -0500

    code complet capteur

--- capteurs/capteur.py
+++ capteurs/capteur.py
@@ -1,13 +1,16 @@
+import Adafruit_DHT as dht
+from time import sleep
 import serial
 import time
 
 ser = serial.Serial("/dev/serial0", baudrate=9600, timeout=0.5)
-print("Debut du capteur de CO2 par UART\n")
 ser.flushInput()
 
-
+DHT = 4
 
 while True:
+    h,t = dht.read_retry(dht.DHT22, DHT)
+    #print("Temp={0:0.1f}*C Humidity={1:0.1f}%".format(t,h))
     ser.flushInput()
     ser.write(b"\xFE\x44\x00\x08\x02\x9F\x25")
     time.sleep(1)
@@ -18,12 +21,5 @@ while True:
         high = resp[3]
         low = resp[4]
         co2 = (high * 256) + low
-        print("CO2 = " + str(co2) + "ppm")
-    #try:
-        
-    
-    #except IndexError:
-    #print("Erreur dans la lecture du capteur de co2")
-    
-    
-    #time.sleep(3)
+        print("CO2 = " + str(co2) + "ppm " + " température : " + str(t) + ", humidité : " + str(h))
+    sleep(3)
