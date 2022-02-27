f6a26e15d578f0b378a97f863cba1d20968bcb98
Michel Desjardins <nitrome264@gmail.com>
2022-02-27 12:38:05 -0500

    basic rename

---
+++ Capteurs/tests/temp+humid.py
@@ -0,0 +1,9 @@
+import Adafruit_DHT as dht
+from time import sleep
+
+DHT = 4
+
+while True:
+    h, t = dht.read_retry(dht.DHT22, DHT)
+    print("Temp={0:0.1f}*C Humidity={1:0.1f}%".format(t, h))
+    sleep(1)
