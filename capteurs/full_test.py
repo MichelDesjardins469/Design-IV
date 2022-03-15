57a3060b8ba9d535ab660622b94e1e001d4a9bf6
olivier Lajoie <olivier.lajoie.2@ulaval.ca>
2022-03-15 10:43:14 -0400

    nettoyage fichiers

--- capteurs/full_test.py
+++ capteurs/full_test.py
@@ -1,25 +1,27 @@
-import Adafruit_DHT as dht
+#import Adafruit_DHT as dht
 from time import sleep
 import serial
 
 DHT = 4
-ser_co = serial.Serial("/dev/serial0", baudrate=9600, timeout=0.5)
-ser_co.flushInput()
+#ser_co = serial.Serial("/dev/serial0", baudrate=9600, timeout=0.5)
+#ser_co.flushInput()
 
 
 def getValeurs():
-    h, t = dht.read_retry(dht.DHT22, DHT)
-
-    ser_co.flushInput()
-    ser_co.write(b"\xFE\x44\x00\x08\x02\x9F\x25")
-    resp = ser_co.read(7)
+    #h, t = dht.read_retry(dht.DHT22, DHT)
+    h,t=1
     co2 = -1
 
     try:
-        high = resp[3]
-        low = resp[4]
-        co2 = (high * 256) + low
-    except TypeError:
+        i = 1/0
+        #ser_co.flushInput()
+        #ser_co.write(b"\xFE\x44\x00\x08\x02\x9F\x25")
+        #resp = ser_co.read(7)
+        #high = resp[3]
+        #low = resp[4]
+        #co2 = (high * 256) + low
+    except:
+        print(t,h,-1)
         return (t, h, -1)
 
     # envoie info:
@@ -30,7 +32,7 @@ sleep(30)
 
 
 def lecture():
-    time.sleep(3)
+    sleep(3)
     return "THIS IS A TEST\n"
 
 
